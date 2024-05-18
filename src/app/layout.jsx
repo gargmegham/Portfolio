35b1aadb73debea24d08d64c49aff8c282705b23
Megham Garg <95271253+gargmegham@users.noreply.github.com>
2024-05-18 16:59:53 +0530

    added ga4

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -33,6 +33,10 @@ export default function RootLayout({ children }) {
       <Script id="clarity-script" strategy="afterInteractive">
         {`(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");`}
       </Script>
+      <Script id="google-script" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', '${process.env.NEXT_PUBLIC_GA4}');`}
+      </Script>
+      <Script strategy="afterInteractive" async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA4}`}></Script>
       <body className={inter.className}>{children}</body>
     </html>
   );
